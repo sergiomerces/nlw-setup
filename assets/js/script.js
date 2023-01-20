13be58d92d3f97ef2f96590569bc1aaa5caabdd5
sergiomerces <sergio.merces@gmail.com>
2023-01-20 01:05:06 -0300

    implementando função para adicionar dia

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,23 @@
 const form = document.querySelector('#form-habits');
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector('header button');
 
-const data = {
+button.addEventListener('click', add);
+
+function add() {
+    const today = '01/01';
+    const dayExists = nlwSetup.dayExists(today);
+    
+    if(dayExists) {
+        alert('Dia já incluso! 🔴');
+        return;
+    }
+
+    alert('Dia adicionado com sucesso! 🔵')
+    nlwSetup.addDay(today);
+}
+
+/* const data = {
     run: ['01-01', '01-02', '01-06'],
     water: ['01-04', '01-05'],
     food: ['01-04', '01-05'],
@@ -10,4 +26,4 @@ const data = {
 }
 
 nlwSetup.setData(data);
-nlwSetup.load();
+nlwSetup.load(); */
