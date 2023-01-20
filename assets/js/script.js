8fc03a56df5aff1edb42be6787030b78ba27fb6a
sergiomerces <sergio.merces@gmail.com>
2023-01-20 02:09:17 -0300

    armazenamento e recuperação do local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const nlwSetup = new NLWSetup(form);
 const button = document.querySelector('header button');
 
 button.addEventListener('click', add);
+form.addEventListener('change', save);
 
 function add() {
     const today = new  Date().toLocaleDateString('pt-br').slice(0, -5);
@@ -17,13 +18,11 @@ function add() {
     nlwSetup.addDay(today);
 }
 
-/* const data = {
-    run: ['01-01', '01-02', '01-06'],
-    water: ['01-04', '01-05'],
-    food: ['01-04', '01-05'],
-    journal: ['01-03'],
-    takePills: ['01-02']
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data));
 }
 
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {};
 nlwSetup.setData(data);
-nlwSetup.load(); */
+nlwSetup.load();  
+
